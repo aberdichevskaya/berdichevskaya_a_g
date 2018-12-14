69844f24468733123a274f551ec2b5b31c810abe
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-14 14:03:56 +0300

    Update dinamic.cpp

--- labs/dinamic/dinamic.cpp
+++ labs/dinamic/dinamic.cpp
@@ -4,41 +4,36 @@
 #include "dinamic.h"
 
 Dinamic_array::Dinamic_array(int size) //size, poryadok
-	:size_(size)
-{
+	: size_(size) {
 	if (size >= 0)
-		for (int i = 0; i < size; ++i) 
-		{
+		for (int i = 0; i < size; ++i)  {
 			data_[i] = 0;
 		}
-	else
+	else {
 		throw std::invalid_argument("Uncorrect size");
+	}
 }
 
 Dinamic_array::Dinamic_array(const Dinamic_array& data)
-	:size_ (data.size_)
+	: size_ (data.size_)
 {
-	for (int i = 0; i < data.size_; ++i)
-	{
+	for (int i = 0; i < data.size_; ++i) {
 		data_[i] = data[i];
 	}
 }
 
-Dinamic_array::~Dinamic_array()
-{
+Dinamic_array::~Dinamic_array() {
 	delete[] data_;
 	data_= nullptr;
 	size_ = 0;
 }
 
-int Dinamic_array::GetSize() const
-{
+int Dinamic_array::GetSize() const {
 	return size_;
 }
 
 
-const double& Dinamic_array::operator[](int i) const 
-{
+const double& Dinamic_array::operator[](int i) const {
 	if ((i < 0) || (i >= size_)) {
 		throw std::invalid_argument("Uncorrect index");
 	}
@@ -46,8 +41,7 @@ const double& Dinamic_array::operator[](int i) const
 		return data_[i];	
 }
 
-double& Dinamic_array::operator[](int i)
-{
+double& Dinamic_array::operator[](int i) {
 	if ((i < 0) || (i >= size_)) {
 		throw std::invalid_argument("Uncorrect index");
 	}
@@ -56,40 +50,39 @@ double& Dinamic_array::operator[](int i)
 }
 
 
-bool Dinamic_array::operator==(const Dinamic_array& rhs)
-{
+bool Dinamic_array::operator==(const Dinamic_array& rhs) {
 	bool resalt(nullptr);
-	if (size_ != rhs.size_)
-	{
-		resalt = false;
+	if(*this == rhs) {
+		resalt = true;
 	}
-	else
-		for (int i = 0; i < rhs.size_; ++i)
-		{
-			if (data_[i] != rhs[i])
-			{
-				resalt = false;
+	else {
+		if (size_ != rhs.size_) {
+			resalt = false;
+		}
+		else {
+			for (int i = 0; i < rhs.size_; ++i) {
+				if (data_[i] != rhs[i]) {
+					resalt = false;
+				}
 			}
 		}
-	if (resalt != false)
-		resalt = true;
+		if (resalt != false) {
+			resalt = true;
+		}
+	}
 	return resalt;
 }
 
-bool Dinamic_array::operator!=(const Dinamic_array& rhs)
-{
+bool Dinamic_array::operator!=(const Dinamic_array& rhs) {
 	return !operator==(rhs);
 }
 
-Dinamic_array& Dinamic_array::operator=(const Dinamic_array& rhs) //imena
-{
-	if (*this != rhs)
-	{
+Dinamic_array& Dinamic_array::operator=(const Dinamic_array& rhs) {
+	if (*this != rhs) {
 		delete[] data_;
 		size_ = rhs.size_;
 		data_ = new double [size_];
-		for (int i = 0; i < size_; i+=1)
-		{
+		for (int i = 0; i < size_; i+=1) {
 			data_[i] = rhs.data_[i];
 		}
 	}
