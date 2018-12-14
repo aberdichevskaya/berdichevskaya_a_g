52ef9a89abffc1ad22fb24801c4281cd35e04c28
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-14 14:08:43 +0300

    Update complex.cpp

--- labs/complex/complex.cpp
+++ labs/complex/complex.cpp
@@ -1,29 +1,29 @@
 #include "complex.h"
-#include <iostream>
-#include <sstream>
-bool Complex::operator==(const Complex& rhs)
-{
+
+#include<iostream>
+#include<sstream>
+
+bool Complex::operator==(const Complex& rhs) {
 	return (re == rhs.re) && (im == rhs.im);
 }
-bool Complex::operator!=(const Complex& rhs)
-{
+
+bool Complex::operator!=(const Complex& rhs) {
 	return !operator==(rhs);
 }
-Complex& Complex::operator+=(const double rhs)
-{
+
+Complex& Complex::operator+=(const double rhs) {
 	return operator+=(Complex(rhs));
 }
-Complex& Complex::operator-=(const double rhs)
-{
+
+Complex& Complex::operator-=(const double rhs) {
 	return operator-=(Complex(rhs));
 }
 
-inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs)
-{
+std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) {
 	return rhs.writeTo(ostrm);
 }
-inline std::istream& operator>>(std::istream& istrm, Complex& rhs)
-{
+
+std::istream& operator>>(std::istream& istrm, Complex& rhs) {
 	return rhs.readFrom(istrm);
 }
 
@@ -37,64 +37,54 @@ Complex::Complex(const double real)
 {}
 
 
-Complex& Complex::operator+=(const Complex& rhs)
-{
+Complex& Complex::operator+=(const Complex& rhs) {
 	re += rhs.re;
 	im += rhs.im;
 	return *this;
 }
 
-Complex operator+(const Complex& lhs, const Complex& rhs)  // why don't need &?
-{
+Complex operator+(const Complex& lhs, const Complex& rhs)  {
 	Complex sum(lhs);
 	sum += rhs;
 	return sum;
 }
 
 
-Complex& Complex::operator-=(const Complex& rhs)
-{
+Complex& Complex::operator-=(const Complex& rhs) {
 	re -= rhs.re;
 	im -= rhs.im;
 	return *this;
 }
 
-Complex operator-(const Complex& lhs, const Complex& rhs)  // why don't need &?
-{
+Complex operator-(const Complex& lhs, const Complex& rhs)  {
 	return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
 }
 
-Complex& Complex::operator*=(const double rhs)
-{
+Complex& Complex::operator*=(const double rhs) {
 	re *= rhs;
 	im *= rhs;
 	return *this;
 }
 
-std::ostream& Complex::writeTo(std::ostream& ostrm) const
-{
+std::ostream& Complex::writeTo(std::ostream& ostrm) const {
 	ostrm << leftBrace << re << separator << im << rightBrace;
 	return ostrm;
 }
 
-std::istream& Complex::readFrom(std::istream& istrm)
-{
+std::istream& Complex::readFrom(std::istream& istrm) {
 	char leftBrace(0);
 	char rightBrace(0);
 	char comma(0);
 	double real(0.0);
 	double immaginary(0.0);
 	istrm >> leftBrace >> real >> comma >> immaginary >> rightBrace;
-	if (istrm.good())
-	{
-		if ((leftBrace == Complex::leftBrace) && (rightBrace == Complex::rightBrace) && (comma == Complex::separator))
-		{
+	if (istrm.good()) {
+		if ((leftBrace == Complex::leftBrace) && (rightBrace == Complex::rightBrace) && (comma == Complex::separator)) {
 			re = real;
 			im = immaginary;
 		}
 	}
-	else
-	{
+	else {
 		istrm.setstate(std::ios_base::failbit);
 	}
 	return istrm;
