22c92ecfdf6439f5a9da1c9d1de29a8a23e63a56
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-15 19:54:44 +0300

    Update rational_test.cpp

--- labs/rational/rational_test.cpp
+++ labs/rational/rational_test.cpp
@@ -1,34 +1,49 @@
-#include "rational.h"
 #include <iostream>
 #include<sstream>
 
+#include "rational.h"
 
-bool testParse(const std::string str)
-{
-	using namespace std;
-	istringstream strm(str);
+bool testParse(const std::string str) {
+	std::istringstream strm(str);
 	Rational w;
 	strm >> w;
-	if (strm.good())
-	{
-		cout << "Read succes:" << "<-" << w << endl;
+	if (strm.good()) {
+		std::cout << "Read succes:" << "<-" << w << std::endl;
 	}
-	else
-	{
-		cout << "Read error:" << "<-" << w << endl;
+	else {
+		std::cout << "Read error:" << "<-" << w << std::endl;
 	}
 	return strm.good();
 }
 
 
-int main()
-{
-	using namespace std;
-
-	Rational w;
-	w += Rational(1, 1);
+int main() {
 	testParse("{5/7}");
 	testParse("{5.1/2}");
 	testParse("{4/7");
+
+	Rational w;
+	w += Rational(1, 1);
+	Rational a(1, 2);
+	Rational b(-2, 4);
+	Rational c(3, 5);
+	std::cout << "1/2 - 2/4 = " << a + b << std::endl;
+	std::cout << "1/2 + 2/4 = " << a - b << std::endl;
+	std::cout << "1/2 * (-2/4) = " << a * b << std::endl;
+	std::cout << "1/2 / (-2/4) = " << a / b << std::endl;
+	try {
+		Rational d(5, 0);
+	}
+	catch (std::out_of_range exc) {
+		std::cout << exc.what() << std::endl;
+	}
+
+	try {
+		std::string a("{6/0}");
+		testParse(a);
+	}
+	catch (std::out_of_range exc) {
+		std::cout << exc.what() << std::endl;
+	}
 	return 0;
 }
