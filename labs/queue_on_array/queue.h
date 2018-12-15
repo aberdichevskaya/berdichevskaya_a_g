003fe1462fcd97b9a7c27f2911b9a0acee3e82f5
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-15 14:37:37 +0300

    Update queue.h

--- labs/queue_on_array/queue.h
+++ labs/queue_on_array/queue.h
@@ -5,10 +5,10 @@ class Queue
 {
 
 private:
-	int* i_first{ 0 };
-	int* i_last{ 0 };
+	int* data_ = new int[size_];
+	int* i_first = &data_[0];
+	int* i_last = &data_[0];
 	int size_{ 20 };
-	double* data_ = new double[size_];
 public:
 	Queue() = default;
 	Queue(const Queue& data);
@@ -17,11 +17,14 @@ public:
 	bool operator==(const Queue& rhs);
 	bool operator!=(const Queue& rhs);
 	void Push(int value);
-	void Pop() noexcept;
+	void Pop();
 	int Front();
 	int Back();
 	bool IsEmpty();
+	std::ostream& WriteTo(std::ostream& ostr) const;
 };
+
+std::ostream& operator<<(std::ostream& ostr, const Queue& rhs);
 
 
 #endif
