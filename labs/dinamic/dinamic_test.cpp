2cc423266289501f96e39d57a330eb6221bc1951
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-15 21:06:58 +0300

    Update dinamic_test.cpp

--- labs/dinamic/dinamic_test.cpp
+++ labs/dinamic/dinamic_test.cpp
@@ -2,8 +2,7 @@
 
 #include "dinamic.h"
 
-int main()
-{
+int main() {
 	double data(3.5);
 	Dinamic_array arr1(7);
 	Dinamic_array arr2(3);
@@ -12,8 +11,30 @@ int main()
 		arr2[i] = data + i;
 	arr1 = arr2;
 	arr1 = arr1;
-	for (int i = 0; i < arr1.GetSize(); ++i)
-	{
+	arr1 = arr2 = arr3;
+	std::cout << "arr1 before changing: \n";
+	for (int i = 0; i < arr1.GetSize(); ++i) {
 		std::cout << arr1[i] << " ";
 	}
+	std::cout << "arr1 after changing: \n";
+	arr1[2] = 10.2;
+	for (int i = 0; i < arr1.GetSize(); ++i) {
+		std::cout << arr1[i] << " ";
+	}
+
+	try {
+		Dinamic_array arr5(-2);
+	}
+	catch (std::invalid_argument exc) {
+		std::cout << exc.what() << std::endl;
+	}
+
+	try {
+		int f = arr2[8];
+	}
+	catch (std::invalid_argument exc) {
+		std::cout << exc.what() << std::endl;
+	}
+
+	return 0;
 }
