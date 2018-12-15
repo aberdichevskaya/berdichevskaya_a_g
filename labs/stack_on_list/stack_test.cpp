3532b3c1ce9a8dbe208d3cdfe7ef2cf1991dc207
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-15 15:21:28 +0300

    Update stack_test.cpp

--- labs/stack_on_list/stack_test.cpp
+++ labs/stack_on_list/stack_test.cpp
@@ -2,20 +2,26 @@
 
 #include "stack.h"
 
-int main()
-{
+int main() {
 	StackOnList a;
 	for (int i = 0; i < 10; i += 1) {
 		a.Push(i*i);
 	}
-	std::cout << "Stack a: \n" << a << std::endl;
+	std::cout << "Stack a: " << a << std::endl;
 	StackOnList b(a);
+	std::cout << "Top of b: " << b.Top() << std::endl;
 	b.Pop();
-	int h = b.Top();
-	std::cout << h << std::endl;
-	std::cout << "Stack b: \n" << b << std::endl;
+	std::cout << "Top of b: " << b.Top() << std::endl;
+	std::cout << "Stack b: " << b << std::endl;
 	b.Clear();
 	if (b.IsEmpty()) {
 		std::cout << "b is clear";
 	}
+	try {
+		b.Pop();
+	}
+	catch (std::logic_error exc) {
+		std::cout << "Logic error: " << exc.what() << std:: endl;
+	}
+	return 0;
 }
