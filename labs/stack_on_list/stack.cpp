acc97b30a930a0149193e26bf12539a430e8faae
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-16 12:23:41 +0300

    Update stack.cpp

--- labs/stack_on_list/stack.cpp
+++ labs/stack_on_list/stack.cpp
@@ -2,77 +2,72 @@
 
 #include "stack.h"
 
+StackOnList::StackOnList(const StackOnList& rhs) {
+	Node* copy = rhs.data_;
+	data_ = new Node;
+	Node* p = data_;
+	while (copy != nullptr) {
+		p->data = copy->data;
+		copy = copy->next;
+		if (copy != nullptr) {
+			p->next = new Node;
+			p = p->next;
+		}
+	}
+	data_ = p;
+}
 
-StackOnList::~StackOnList(){
+StackOnList::~StackOnList() {
 	Clear();
 }
 
-void StackOnList::Push(const int value){
+void StackOnList::Push(const int value) {
 	data_ = new Node(data_, value);
 }
 
-void StackOnList::Pop() noexcept{
+void StackOnList::Pop() noexcept {
 	Node* deleted(data_);
 	data_ = data_ -> next;
 	delete deleted;
 }
 
-bool StackOnList::IsEmpty(){
-	bool resalt(nullptr);
-	if (data_) { resalt = false;}
-	else { resalt = true; }
-	return resalt;
+bool StackOnList::IsEmpty() {
+	return (data_);
 }
 
-void StackOnList :: Clear()noexcept{
-	while (this->IsEmpty() == false){
+void StackOnList :: Clear() noexcept {
+	while (!IsEmpty()) {
 		Pop();
 	}
 }
 
-int StackOnList::Top(){
-	if (this->IsEmpty()) {
-		throw std::logic_error("Try to get top from empty stack.");
+int StackOnList::Top() {
+	if (IsEmpty()) {
+		throw std::logic_error("Try to get top from empty stack");
 	}
 	return data_->data;
 }
-
-std::ostream& StackOnList::WriteTo(std::ostream&ostr) const {
-	Node* printed(data_);
-	while (printed) {
-		ostr << printed->data;
-		printed = printed->next;
-		if (printed) ostr << ", ";
-	}
-	return ostr;
-}
-
 
 bool StackOnList::operator==(const StackOnList& obj) {
 	bool answer(true);
 	Node* comparing1(this->data_);
 	Node* comparing2(obj.data_);
 	while (comparing1 && comparing2 && answer) {
-		if (comparing1->data != comparing2->data) { 
-			answer = false; }
+		if (comparing1->data != comparing2->data) {
+			answer = false;
+		}
 		comparing1 = comparing1->next;
 		comparing2 = comparing2->next;
 	}
 	return answer;
 }
 
-
 bool StackOnList::operator!=(const StackOnList& obj) {
 	return !(operator==(obj));
 }
 
-StackOnList::StackOnList(const StackOnList& rhs) {
-
-}
-
-StackOnList& StackOnList::operator=(StackOnList& obj)
-{
-	if(*this != obj){
+StackOnList& StackOnList::operator=(StackOnList& obj) {
+	if (*this != obj) {
 		this->Clear();
 		StackOnList first;
 		while (!obj.IsEmpty()) {
@@ -89,4 +84,21 @@ StackOnList& StackOnList::operator=(StackOnList& obj)
 	else {
 		return *this;
 	}
+}
+
+std::ostream& StackOnList::WriteTo(std::ostream&ostr) const {
+	Node* printed(data_);
+	while (printed) {
+		ostr << printed->data;
+		printed = printed->next;
+		if (printed) {
+			ostr << ", ";
+		}
+	}
+	return ostr;
+}
+
+
+std::ostream& operator <<(std::ostream& ostr, const StackOnList& rhs) {
+	return rhs.WriteTo(ostr);
 }
