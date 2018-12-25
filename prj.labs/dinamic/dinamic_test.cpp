9ceeba6eae1a88172bf09e921beb515738ed2259
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2018-12-25 13:06:41 +0300

    Update dinamic_test.cpp

--- prj.labs/dinamic/dinamic_test.cpp
+++ prj.labs/dinamic/dinamic_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <conio.h>
 
 #include "dinamic.h"
 
@@ -8,33 +9,39 @@ int main() {
 	Dinamic_array arr2(3);
 	Dinamic_array arr3(arr2);
 	for (int i = 0; i < arr2.GetSize(); ++i)
-		arr2[i] = data + i;
+		arr2[i] = data + i*1.0;
 	arr1 = arr2;
 	arr1 = arr1;
-	arr1 = arr2 = arr3;
 	std::cout << "arr1 before changing: \n";
 	for (int i = 0; i < arr1.GetSize(); ++i) {
 		std::cout << arr1[i] << " ";
 	}
+	std::cout << std::endl;
 	std::cout << "arr1 after changing: \n";
 	arr1[2] = 10.2;
 	for (int i = 0; i < arr1.GetSize(); ++i) {
 		std::cout << arr1[i] << " ";
 	}
-
+	std::cout << std::endl;
+	arr1 = arr2 = arr3;
+	std::cout << "arr1 after new changing: \n";
+	for (int i = 0; i < arr1.GetSize(); ++i) {
+		std::cout << arr1[i] << " ";
+	}
+	std::cout << std::endl;
 	try {
 		Dinamic_array arr5(-2);
 	}
-	catch (std::invalid_argument& exc) {
+	catch (std::invalid_argument exc) {
 		std::cout << exc.what() << std::endl;
 	}
 
 	try {
 		int f = arr2[8];
 	}
-	catch (std::invalid_argument& exc) {
+	catch (std::invalid_argument exc) {
 		std::cout << exc.what() << std::endl;
 	}
-
+	_getch();
 	return 0;
 }
