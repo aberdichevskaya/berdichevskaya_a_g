fbc3ca814f3efb02e2bfe8f59c9ffae2967bf16c
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2019-01-19 17:33:56 +0300

    Update cells.cpp

--- coursework/work/cells.cpp
+++ coursework/work/cells.cpp
@@ -3,26 +3,31 @@
 
 #include "cells.h"
 
-// Параметры:
-//  text - текст на кнопке
-//  parent - окно, в котором кнопка будет расположена
-//    для корретного удаления кнопки
-//  row - строка, col - столбец на поле
-//  game - ссылка на игру
-Cells::Cells(const char* text, QWidget* parent, int row, int col,game_state& game)
-    : QPushButton(text, parent)
-    , row(row)
-    , col(col)
-    , game(&game) {
+Cells::Cells(const char* text, QWidget* parent, int row, int col, game_state& game)
+	: QPushButton(text, parent)
+	, row(row)
+	, col(col)
+	, game(&game) {
+
 }
 
 // При нажатии на клетку
 void Cells::slotCellClicked() {
-  QString a = game->makeMove(row, col);
-  if ((a == "Первый игрок сделал ход") || (a == "Второй игрок сделал ход")) {
-	  setEnabled(false);
-  }
-  else {
-	  QMessageBox::warning(this, "Аккуратнее!", a);
-  }
+	QString a = game->makeMove(row, col);
+
+	if (a == "Первый игрок сделал ход") {
+		setStyleSheet("color: green");
+		setProperty("currentState", 1);
+		setEnabled(false);
+
+	}
+	else if (a == "Второй игрок сделал ход") {
+		setStyleSheet("color: red");
+		setProperty("currentState", 0);
+		setEnabled(false);
+
+	}
+	else {
+		QMessageBox::warning(this, "Аккуратнее!", a);
+	}
 }
