3e0cbdec0d5078c0f99f9de03604114a3c49ae3e
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2019-01-19 17:35:58 +0300

    Update game_state.h

--- coursework/work/game_state.h
+++ coursework/work/game_state.h
@@ -8,30 +8,35 @@
 #include <QString>
 
 enum State {
-  first_move,
-  second_move,
-  first_win,
-  second_win,
-  draw
+	first_move,
+	second_move,
+	first_win,
+	second_win,
+	draw
 };
 
 class game_state {
 private:
-    int first_score {0};
-    int second_score{0};
-    State state;
+	int first_score{ 0 };
+	int second_score{ 0 };
+	State state;
 	QPair <int, int> last = qMakePair(-1, -1);
 public:
 	std::vector<std::vector<int>> Map;
-    game_state();
-    void mapResize(int h, int w); // Присвоение карте размерa
-    void newGame(int h, int w); // Новая игра без пересоздания объекта
-    QString getStateString(); //состояниe игры
-    QString makeMove(int row, int col);  // Совершаем ход
-    QString checkGameOver();  // Проверяем на окончание игры
-    const char* getCell(int i, int j);   // Состояние ячейки
-    int ret1{0};
-    int ret2{0};
-  };
+	std::vector<std::vector<int>> Moves;
+	game_state();
+	void mapResize(int h, int w); // Присвоение карте размерa
+	void newGame(int h, int w); // Новая игра без пересоздания объекта
+	QString getStateString(); //состояниe игры
+	QString makeMove(int row, int col);  // Совершаем ход
+	QString checkGameOver();  // Проверяем на окончание игры
+	const char* getCell(int i, int j);   // Состояние ячейки
+	void save(QString fileName, int h, int w); //Сохранение игры
+	void load(QString fileName);
+	int ret1{ 0 };
+	int ret2{ 0 };
+	int hh{ 4 };
+	int ww{ 4 };
+};
 
 #endif // GAME_STATE_H
