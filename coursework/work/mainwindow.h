9f9a76751de3130c7a4c4e5aae9776c0a6c346fd
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2019-01-20 21:45:53 +0300

    Update mainwindow.h

--- coursework/work/mainwindow.h
+++ coursework/work/mainwindow.h
@@ -3,12 +3,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-#include <QVector>  //стоит использовать его или std::vector?
+#include <QVector>  
 #include <QDialog>
 
 #include "cells.h"
 #include "game_state.h"
-
+//основное окно иры
 namespace Ui {
 	class MainWindow;
 }
@@ -20,25 +20,25 @@ class MainWindow : public QMainWindow
 public:
 	explicit MainWindow(QWidget *parent = 0);
 	~MainWindow();
-	void resize_buttons();
+	void resize_buttons(); //изменение размера игрового поля
 	void exit_application();
-	int h{ 4 };
-	int w{ 4 };
+	int h{ 4 }; //количество строк игрового поля, умолчательное - 4
+	int w{ 4 }; //количество столбцов игрового поля, умочательное - 4
 
 private slots:
-	void status_string();
-	void newGame();
-	void on_getSize_clicked();
-	void on_exit_game_triggered();
-	void on_new_new_game_triggered();
-	void on_saveGame_clicked();
-	void on_loadGame_clicked();
+	void status_string(); //состояние игрового поля после совершения каждого хода
+	void newGame(); //запуск новой игры без перезапуска программы
+	void on_getSize_clicked(); //вызов диалогового окна для ввода нового размера
+	void on_exit_game_triggered(); //выход из игры
+	void on_new_new_game_triggered(); //перезапуск игры
+	void on_saveGame_clicked(); //сохранение текущей игры
+	void on_loadGame_clicked(); //загрузка сохранённой игры
 
 private:
 	Ui::MainWindow *ui;
-	game_state game;
-	std::vector<std::vector<Cells*>> cells_;
-	void update_buttons();
+	game_state game; //состояние и ход игры
+	std::vector<std::vector<Cells*>> cells_; //игровое поле, состоящее из клеток-кнопок
+	void update_buttons(); //создание/изменение поля клеток
 };
 
 #endif // MAINWINDOW_H
