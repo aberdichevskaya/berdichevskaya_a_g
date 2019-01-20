f737c79a4ed19ce7ffe02a86e1b1ce000f89721d
aberdichevskaya <43777726+aberdichevskaya@users.noreply.github.com>
2019-01-20 21:36:04 +0300

    Update field_size.h

--- coursework/work/field_size.h
+++ coursework/work/field_size.h
@@ -3,7 +3,7 @@
 
 #include <QWidget>
 #include <QDialog>
-
+//размер игрового поля, вводится через диалоговое окно, описанное в этом классе
 namespace Ui {
     class field_size;
 }
@@ -14,11 +14,11 @@ class field_size : public QDialog {
 public:
     explicit field_size(QWidget *parent = 0);
     ~field_size();
-    int H{4};
-    int W{4};
+    int H{4}; //количество строк в игровом поле, умолчательное значение - 4
+    int W{4}; //количество столбцов в поле, умолчательное значение - 4
 
 private slots:
-    void on_start_clicked();
+    void on_start_clicked(); //нажатие на кнопку - перезапуск игры с новым размером поля
 
 private:
     Ui::field_size *ui;
